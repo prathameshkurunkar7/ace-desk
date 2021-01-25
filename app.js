e26f03030906a2a6a7dbcb25319237f6dfdd6d8b
Prathamesh Kurunkar <kurunkarprathamesh7@gmail.com>
2021-01-25 19:43:11 +0530

    Deploying Trials

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ app.use('/employee',employeeRouter);
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'))
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
